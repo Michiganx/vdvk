295c56e7bd8ce204ccae8ce03339355c22d4860c
Michiganx <michigan@ukr.net>
2015-08-04 22:44:06 +0300

    vk auth alpha

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,34 @@
+class StaticPagesController < ApplicationController
+  def index
+    # сначала создадим клиент API
+    vk = VkontakteApi::Client.new(session[:token])
+
+    # теперь получим текущего юзера
+    @user = vk.users.get(uid: session[:vk_id], fields: [:screen_name, :photo]).first
+
+    # его друзей
+    @friends = vk.friends.get(fields: [:screen_name, :sex, :photo, :last_seen])
+    # отдельно выберем тех, кто в данный момент онлайн
+    @friends_online = @friends.select { |friend| friend.online == 1 }
+
+    # группы
+    @groups = vk.groups.get(extended: 1)
+    # первый элемент массива - кол-во групп; его нужно выкинуть
+    @groups.shift
+
+    # и ленту новостей
+    raw_feed = vk.newsfeed.get(filters: 'post')
+    # обработанную в отдельном методе
+    @newsfeed = process_feed(raw_feed)
+
+  end
+
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+end
---
+++ config/initializers/vkontakte_api.rb
@@ -0,0 +1,28 @@
+VkontakteApi.configure do |config|
+  # Authorization parameters (not needed when using an external authorization):
+  # config.app_id       = '5020822'
+  # config.app_secret   = '33JQffS1sjB7WcAJbzh5'
+  # config.redirect_uri = 'vdolgah.herokuapp.com/oauth/callback'
+  
+  # Faraday adapter to make requests with:
+  # config.adapter = :net_http
+  
+  # HTTP verb for API methods (:get or :post)
+  # config.http_verb = :get
+  
+  # Logging parameters:
+  # log everything through the rails logger
+  config.logger = Rails.logger
+  
+  # log requests' URLs
+  # config.log_requests = true
+  
+  # log response JSON after errors
+  # config.log_errors = true
+  
+  # log response JSON after successful responses
+  # config.log_responses = false
+end
+
+# create a short alias VK for VkontakteApi module
+# VkontakteApi.register_alias
