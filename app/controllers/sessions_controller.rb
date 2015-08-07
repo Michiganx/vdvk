6bdae07e5c3ef723fcb3c86a98b0d2aaae1f35db
Michiganx <michigan@ukr.net>
2015-08-07 20:14:54 +0300

    auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,25 @@
 class SessionsController < ApplicationController
   def new
+
+    if session[:state].present? && session[:state] != params[:state]
+      redirect_to root_url, alert: 'Ошибка авторизации, попробуйте войти еще раз.' and return
+    else
+
+    # получение токена
+    @vk = VkontakteApi.authorize(code: params[:code])
+    # и сохранение его в сессии
+    session[:token] = @vk.token
+    # также сохраним id пользователя на ВКонтакте - он тоже пригодится
+    session[:vk_id] = @vk.user_id
+
+    redirect_to root_url
+
+
     srand
     session[:state] ||= Digest::MD5.hexdigest(rand.to_s)
     #URL страницы авторизации
     @vk_url = VkontakteApi.authorization_url(scope: [:friends, :groups, :offline, :notify], state: session[:state])
+      end
   end
 
   def callback
